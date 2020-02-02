b96ce6595b4c5cd5631778a7b3aa52ac96717cb0
Irina Kukuyeva <ikukuyeva@gmail.com>
2020-02-01 16:44:42 -0800

    network.py: Address linter feedback

--- Class4/network.py
+++ Class4/network.py
@@ -32,14 +32,17 @@ def sigmoid_prime(z):
     return sigmoid(z) * (1 - sigmoid(z))
 
 
-class Network(object):
+class Network():
+    """Neural network implemented in base Python, which is easier to debug and
+       understand what's going on.
+    """
     def __init__(self, sizes):
         """The list ``sizes`` contains the number of neurons in the
            respective layers of the network.  For example, if the list
            was [2, 3, 1] then it would be a three-layer network, with the
            first layer containing 2 neurons, the second layer 3 neurons,
-           and the third layer 1 neuron.  
-        
+           and the third layer 1 neuron.
+           ---
            The biases and weights for the network are initialized randomly,
            using a Gaussian distribution with mean 0, and variance 1.
            - Note that the first layer is assumed to be an input layer, and by
@@ -128,7 +131,7 @@ class Network(object):
     def evaluate(self, test_data, accuracy=True):
         """Return overall network accuracy or probabilities of each neuron given
            by the network.
-		   
+		   ---
 		   TODO: Candidate for refactoring as uses if/else statement w/ output.
         """
         # For each labeled image in the data set, see which class the network
@@ -147,4 +150,4 @@ class Network(object):
         """Return the vector of partial derivatives \partial C_x /
            \partial a for the output activations.
         """
-        return (output_activations-y)
+        return output_activations-y
