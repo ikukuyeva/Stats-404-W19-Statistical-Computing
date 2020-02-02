6fe692e34a0eab0a00443d4eb9add3cf40b1e16d
Irina Kukuyeva <ikukuyeva@gmail.com>
2020-02-01 16:22:23 -0800

    network.py: Add comments to make it more clear what the code is doing

--- Class4/network.py
+++ Class4/network.py
@@ -35,15 +35,19 @@ def sigmoid_prime(z):
 class Network(object):
     def __init__(self, sizes):
         """The list ``sizes`` contains the number of neurons in the
-        respective layers of the network.  For example, if the list
-        was [2, 3, 1] then it would be a three-layer network, with the
-        first layer containing 2 neurons, the second layer 3 neurons,
-        and the third layer 1 neuron.  The biases and weights for the
-        network are initialized randomly, using a Gaussian
-        distribution with mean 0, and variance 1.  Note that the first
-        layer is assumed to be an input layer, and by convention we
-        won't set any biases for those neurons, since biases are only
-        ever used in computing the outputs from later layers."""
+           respective layers of the network.  For example, if the list
+           was [2, 3, 1] then it would be a three-layer network, with the
+           first layer containing 2 neurons, the second layer 3 neurons,
+           and the third layer 1 neuron.  
+        
+           The biases and weights for the network are initialized randomly,
+           using a Gaussian distribution with mean 0, and variance 1.
+           - Note that the first layer is assumed to be an input layer, and by
+             convention we won't set any biases for those neurons, since biases
+             are only ever used in computing the outputs from later layers.
+           - Note that there are no weights in the last layer, as it's the
+             output layer.
+        """
         random.seed(2019)
         self.num_layers = len(sizes)
         self.sizes = sizes
@@ -59,48 +63,56 @@ class Network(object):
 
     def SGD(self, training_data, learning_rate):
         """Train the neural network using gradient descent. ``training_data``
-        is a list of tuples ``(x, y)`` representing the training inputs and
-        the desired outputs.  The other non-optional parameters are
-        self-explanatory."""
+           is a list of tuples ``(x, y)`` representing the training inputs and
+           the desired outputs.
+        """
         accuracy_new = 0
         while accuracy_new < 0.8:
             accuracy_old = accuracy_new
             nabla_b = [np.zeros(b.shape) for b in self.biases]
             nabla_w = [np.zeros(w.shape) for w in self.weights]
+            # --- Iterate over each labeled image in the data set:
             for x, y in training_data:
+            	# --- Find small updates to weights and biases that may result
+            	#     in better fit:
                 delta_nabla_b, delta_nabla_w = self.backprop(x, y)
                 nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
                 nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+            # --- Update weights and biases:
             self.weights = [w-(learning_rate/len(training_data))*nw
                             for w, nw in zip(self.weights, nabla_w)]
             self.biases = [b-(learning_rate/len(training_data))*nb
                            for b, nb in zip(self.biases, nabla_b)]
+            # --- Evaluate model fit:
             true_positives = self.evaluate(training_data)
             accuracy_new = float(true_positives)/len(training_data)
             print(accuracy_new)
             if abs(accuracy_new - accuracy_old) < 0.00001:
+            	# --- Stopping criteria, if model accuracy does not change:
                 break
         return self.evaluate(training_data, accuracy=False)
 
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
-        gradient for the cost function C_x.  ``nabla_b`` and
-        ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
-        to ``self.biases`` and ``self.weights``."""
+           gradient for the cost function C_x.  ``nabla_b`` and
+           ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
+           to ``self.biases`` and ``self.weights``.
+        """
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
-        # feedforward
+        # --- Feedforward (through each layer, starting from the first):
         activation = x
         activations = [x] # list to store all the activations, layer by layer
-        zs = [] # list to store all the z vectors, layer by layer
+        zs = [] 		  # list to store all the z vectors, layer by layer,
+        				  #      where z is the "value" of node, before it's
+        				  #      passed to activation fcn
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        # backward pass
-        delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+        # --- Backward pass (through each layer, starting from the last):
+        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Here, l = 1 means the last layer of neurons, l = 2 is second-to-last
@@ -111,18 +123,29 @@ class Network(object):
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
+        # Find small updates to weights and biases that may result in better fit:
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data, accuracy=True):
-        """Return accuracy or final predictions."""
+        """Return overall network accuracy or probabilities of each neuron given
+           by the network.
+		   
+		   TODO: Candidate for refactoring as uses if/else statement w/ output.
+        """
+        # For each labeled image in the data set, see which class the network
+        # predicts, and which class is the labeled class:
         test_results = [(np.argmax(self.feedforward(x)), np.argmax(y)) for
                         (x, y) in test_data]
         if accuracy:
+        	# Compare predicted and observed labels:
             return sum(int(y_pred == y) for (y_pred, y) in test_results)
         else:
+        	# Return probabilities associated with each neuron, rather than
+        	# labels:
             return [self.feedforward(x) for (x, y) in test_data]
 
     def cost_derivative(self, output_activations, y):
         """Return the vector of partial derivatives \partial C_x /
-        \partial a for the output activations."""
+           \partial a for the output activations.
+        """
         return (output_activations-y)
