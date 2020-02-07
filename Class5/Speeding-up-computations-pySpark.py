b52405161187e151513336db07e937987d9ffd80
Irina Kukuyeva <ikukuyeva@gmail.com>
2020-02-07 14:21:29 -0800

    Databricks notebook: make comments more clear
    NOTE: Reading-in from URL to spark dataframe via pandas did not seem to work with SQL view; cluster got disconnected

--- Class5/Speeding-up-computations-pySpark.py
+++ Class5/Speeding-up-computations-pySpark.py
@@ -7,13 +7,12 @@ file_name = "https://s3.amazonaws.com/h2o-airlines-unpacked/year2012.csv"
 
 # COMMAND ----------
 
+### --- Read-in dataset by first moving file to cluster's local storage
 # Download file, per https://docs.databricks.com/_static/notebooks/zip-files-python.html:
 urllib.request.urlretrieve(file_name, '/tmp/df.csv')
 
 # Move file per to DBFS, per https://docs.databricks.com/_static/notebooks/zip-files-python.html:
 dbutils.fs.mv("file:/tmp/df.csv", "dbfs:/data/df.csv")
-
-# COMMAND ----------
 
 # Per https://spark.apache.org/docs/2.2.0/sql-programming-guide.html#datasets-and-dataframes, 'spark' is an existing SparkSession:
 df_spark = spark.read.format('csv').options(header='true', inferSchema='true').load('dbfs:/data/df.csv')
@@ -24,7 +23,7 @@ display(df_spark)
 
 # COMMAND ----------
 
-# Make dataframe available to run SQL queries against:
+# --- Make dataframe available to run SQL queries against:
 df_spark.createOrReplaceTempView("df_spark")
 
 # COMMAND ----------
