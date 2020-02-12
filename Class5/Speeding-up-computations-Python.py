44da187c6e83daf16e5143be50ae371fa38ea5ca
Irina Kukuyeva <ikukuyeva@gmail.com>
2020-02-12 15:53:30 -0800

    Fix bug in function by recoding missing values to -9999, such that comparison can happen

--- Class5/Speeding-up-computations-Python.py
+++ Class5/Speeding-up-computations-Python.py
@@ -305,7 +305,7 @@ type(df['ArrDelay'].values)
 # In[118]:
 
 
-get_ipython().run_cell_magic('time', '', "df['compensated_delays_vec_np'] = delays_requiring_compensation_vec(df['ArrDelay'].values, \n                                                                    df['DepDelay'].values)")
+get_ipython().run_cell_magic('time', '', '# Redo recoding of missing values, per \n# https://stackoverflow.com/questions/54922775/using-magic-command-timeit-n1-r1-causes-jupyter-does-not-keep-the-value-of\ndf["DepDelay"] = df["DepDelay"].fillna(-9999)\ndf["ArrDelay"] = df["ArrDelay"].fillna(-9999)\ndf[\'compensated_delays_vec_np\'] = delays_requiring_compensation_vec(df[\'ArrDelay\'].values, \n                                                                    df[\'DepDelay\'].values)')
 
 
 # In[119]:
